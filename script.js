4bd88b4fd61e1e0b8d29882b3c702c5c89c5132d
lokeshlk368 <lk689735@gmail.com>
2020-04-14 21:37:25 +0530

    files added

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-\
+// var x = document.getElementsByClassName("fadeProperty");
+// x.addEventListener('mouseover',function(){
+//   $(".commomImageSection h1").fadeIn("slow");
+// });
+// x.addEventListener('mouseout',function(){
+//   $(".commomImageSection h1").fadeOut(3000);
+// });
+
+// $(".fadeProperty").mouseover(function(){
+//   $(".commomImageSection h1").fadeIn("slow");
+// });
+// $(".fadeProperty").mouseout(function(){
+//   $(".commomImageSection h1").fadeOut(3000);
+// })
